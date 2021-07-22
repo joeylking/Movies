01e42e52e09ba004ada0e36cbb33d752fa8479b2
Joey <30815220+joeylking@users.noreply.github.com>
2021-07-22 07:24:54 -0500

    Routing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import RMDBLogo from "../../images/react-movie-logo.svg";
+import { Link } from "react-router-dom";
 
 import Movies from "../../images/udhZ1e01.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
@@ -10,8 +9,12 @@ import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={Movies} alt="movies" />
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      <Link to="/">
+        <LogoImg src={Movies} alt="movies" />
+      </Link>
+      <a target="_blank" href="https://www.themoviedb.org/">
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </a>
     </Content>
   </Wrapper>
 );
