2eba6a37a5479e08bd9fe835cdd83dc21db1ad13
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-17 14:41:33 -0500

    MovieInfo Styles

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import Thumb from "../Thumb";
+import Thumb from '../Thumb';
 
-import { IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
 
-import NoImage from "../../images/no_image.jpg";
+import NoImage from '../../images/no_image.jpg';
 
-import { Wrapper, Content, Text } from "../MovieInfo/MovieInfo.styles";
+import { Wrapper, Content, Text } from '../MovieInfo/MovieInfo.styles';
 
 const MovieInfo = ({ movie }) => (
   <Wrapper backdrop={movie.backdrop_path}>
@@ -18,20 +18,20 @@ const MovieInfo = ({ movie }) => (
             : NoImage
         }
         clickable={false}
-        alt="movie-thumb"
+        alt='movie-thumb'
       />
       <Text>
         <h1>{movie.title}</h1>
         <h3>PLOT</h3>
         <p>{movie.overview}</p>
 
-        <div className="rating-directors">
+        <div className='rating-directors'>
           <div>
             <h3>RATING</h3>
-            <div className="score">{movie.vote_average}</div>
+            <div className='score'>{movie.vote_average}</div>
           </div>
-          <div className="director">
-            <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
+          <div className='director'>
+            <h3>DIRECTOR{movie.directors.length > 1 ? 'S' : ''}</h3>
             {movie.directors.map(director => (
               <p key={director.credit_id}>{director.name}</p>
             ))}
